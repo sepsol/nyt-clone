07cc765367281757f975ee9ac67bf51246b1388d
SepSol <SR-75@live.com>
2020-08-30 01:20:29 -0700

    added noscript tag

---
+++ header.js
@@ -0,0 +1,39 @@
+var lastScrollTop = 0;
+// element should be replaced with the actual target element on which you have applied scroll, use window in case of no target element.
+window.addEventListener(
+  'scroll',
+  function () {
+    // or window.addEventListener("scroll"....
+    var st = window.pageYOffset || document.documentElement.scrollTop; // Credits: "https://github.com/qeremy/so/blob/master/so.dom.js#L426"
+    if (st > lastScrollTop) {
+      // downscroll code
+      alert('down');
+    } else {
+      // upscroll code
+      alert('up');
+    }
+    lastScrollTop = st <= 0 ? 0 : st; // For Mobile or negative scrolling
+  },
+  false
+);
+
+/*
+
+=== NOTE TO SELF ===
+
+// how to detect scroll direction
+var lastScrollTop = 0;
+// element should be replaced with the actual target element on which you have applied scroll, use window in case of no target element.
+window.addEventListener("scroll", function(){ // or window.addEventListener("scroll"....
+   var st = window.pageYOffset || document.documentElement.scrollTop; // Credits: "https://github.com/qeremy/so/blob/master/so.dom.js#L426"
+   if (st > lastScrollTop){
+      // downscroll code
+			alert('down')
+   } else {
+      // upscroll code
+			alert('up')
+   }
+   lastScrollTop = st <= 0 ? 0 : st; // For Mobile or negative scrolling
+}, false);
+
+*/

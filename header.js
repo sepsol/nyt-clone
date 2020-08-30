5735776098277aff9255c281b51713bc4473bfbe
SepSol <SR-75@live.com>
2020-08-30 01:23:29 -0700

    changed site description

--- header.js
+++ header.js
@@ -1,3 +1,6 @@
+const header = document.querySelector('header');
+const h1 = document.querySelector('h1');
+
 var lastScrollTop = 0;
 // element should be replaced with the actual target element on which you have applied scroll, use window in case of no target element.
 window.addEventListener(
